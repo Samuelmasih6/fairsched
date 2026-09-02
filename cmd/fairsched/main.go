ade7e128370a03f22e5745431181f2c53c90f026
Samuel Masih <sidhusamuel567@gmail.com>
2026-09-02 23:09:56 +0530

    reflect changes <-- error handling

--- cmd/fairsched/main.go
+++ cmd/fairsched/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"time"
+	"fmt"
 
 	"github.com/Samuelmasih6/fairsched/internal/job"
 	"github.com/Samuelmasih6/fairsched/internal/scheduler"
@@ -12,45 +12,50 @@ func main() {
 
 	s.Start(3)
 
-	s.Submit(job.Job{
-		ID:       "job-001",
-		TenantID: "tenant-a",
-		Priority: 1,
-		Payload:  "Low priority job",
-		Duration: 5 * time.Second,
-	})
+	jobs := []job.Job{
+		{
+			ID:       "job-001",
+			TenantID: "tenant-a",
+			Priority: 1,
+			Payload:  "Low priority job",
+			Duration: 5,
+		},
+		{
+			ID:       "job-002",
+			TenantID: "tenant-a",
+			Priority: 10,
+			Payload:  "Highest priority job",
+			Duration: 1,
+		},
+		{
+			ID:       "job-003",
+			TenantID: "tenant-b",
+			Priority: 5,
+			Payload:  "Medium priority job",
+			Duration: 3,
+		},
+		{
+			ID:       "job-004",
+			TenantID: "tenant-b",
+			Priority: 3,
+			Payload:  "Low-medium priority job",
+			Duration: 2,
+		},
+		{
+			ID:       "job-005",
+			TenantID: "tenant-a",
+			Priority: 8,
+			Payload:  "High priority job",
+			Duration: 7,
+		},
+	}
 
-	s.Submit(job.Job{
-		ID:       "job-002",
-		TenantID: "tenant-a",
-		Priority: 10,
-		Payload:  "Highest priority job",
-		Duration: 1 * time.Second,
-	})
-
-	s.Submit(job.Job{
-		ID:       "job-003",
-		TenantID: "tenant-b",
-		Priority: 5,
-		Payload:  "Medium priority job",
-		Duration: 3 * time.Second,
-	})
-
-	s.Submit(job.Job{
-		ID:       "job-004",
-		TenantID: "tenant-b",
-		Priority: 3,
-		Payload:  "Low-medium priority job",
-		Duration: 2 * time.Second,
-	})
-
-	s.Submit(job.Job{
-		ID:       "job-005",
-		TenantID: "tenant-a",
-		Priority: 8,
-		Payload:  "High priority job",
-		Duration: 7 * time.Second,
-	})
+	for _, j := range jobs {
+		if err := s.Submit(j); err != nil {
+			fmt.Println("failed to submit job:", err)
+		}
+	}
 
 	s.Shutdown()
+	s.Wait()
 }
